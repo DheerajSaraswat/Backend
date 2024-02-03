6cbfb47187edde26435453962c4299ca3c7ac4e7
Dheeraj Saraswat <dheeraj200410@gmail.com>
2024-02-04 00:37:45 +0530

    models and file handling

---
+++ src/models/video.models.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      title: Number, // from cloudinary
+      required: true,
+    },
+    views: {
+      title: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+videoSchema.plugin(aggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
